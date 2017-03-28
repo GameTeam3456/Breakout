b406427bb3841945deddc684df31e63d71cc29fd
Ucchan <ucchan.develp@gmail.com>
2017-03-28 14:26:06 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,26 @@ import javafx.stage.Stage;
 public class Main extends Application{
     public static void main(String[] args){
         
-       launch(args);　//アプリケーションの起動
+       launch(args);　//JavaFXのスレッドを起動
        //startメソッドの呼び出しはJavaFXランタイムが自動的に実行
         
     }  
            
      public void start(Stage theStage){
-        theStage.setTitle("Hello, World!");
+        theStage.setTitle("BreakOut");
+         
+         // ルートのコンテナ
+        StackPane root = new StackPane();
+        
+        // シーンの生成
+        Scene scene = new Scene(root, 300, 250);
+
+        // シーンをステージに貼る
+        stage.setScene(scene);
+        
         theStage.show();
+         
+         
     }
 }
     
