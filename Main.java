c0e41b5683fc2e8805dbc04e7c13ae011cde8d5d
Ucchan <ucchan.develp@gmail.com>
2017-03-28 10:00:34 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,19 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+
+public class Main extends Application{
+    public static void main(String[] args){
+        
+       super.launch(args);　　//アプリケーションの起動
+        
+    }  
+           
+     public void start(Stage theStage){
+        theStage.setTitle("Hello, World!");
+        theStage.show();
+    }
+}
+    
+        
+    
