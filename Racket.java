6fa951531a6c83eec07d24e29ba07e8f942741ad
Ucchan <ucchan.develp@gmail.com>
2017-04-01 18:27:31 +0900

    Create Racket.java

---
+++ Racket.java
@@ -0,0 +1,46 @@
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Racket{
+	
+	//ラケットのサイズ
+	public static final int WIDTH = 80;
+	public static final int HEIGHT = 5;
+	
+	//ラケットの中心位置
+	private int centerPos;
+	
+	public Racket(){
+		// ラケットの位置を画面の真ん中で初期化
+        centerPos = MainPanel.WIDTH / 2;
+	}
+	
+	/**
+     * ラケットの描画
+     * 
+     * @param g
+     */
+     
+     public void draw(Graphics g){
+     	g.setColor(Color.WHITE);
+     	g.fillRect(centerPos - WIDTH / 2 , MainPanel.HEIGHT - HEIGHT, WIDTH, HEIGHT);
+     }    
+     
+      /**
+     * ラケットの移動
+     * 
+     * @param pos 移動先X座標
+     */
+     
+     public void move(int pos){
+     	centerPos = pos;
+     	
+     
+     if (centerPos < WIDTH / 2) { // 左端
+            centerPos = WIDTH / 2;
+        } else if (centerPos > MainPanel.WIDTH - WIDTH / 2) { // 右端
+            centerPos = MainPanel.WIDTH - WIDTH / 2;
+        }
+    }
+   }
+    
