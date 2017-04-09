5918434601fcdb071e47144ea66dc645ffd0fb60
satosato0502 <kobuta.0502.sato@gmail.com>
2017-04-09 23:16:46 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,10 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application{
+    
+    static int scene_takasa = 300;
+	static int scene_haba = 250;
+    
     public static void main(String[] args){
         
        launch(args);　//JavaFXのスレッドを起動
@@ -12,11 +16,20 @@ public class Main extends Application{
      public void start(Stage theStage){
         theStage.setTitle("BreakOut");
          
+         theStage.setWidth(scene_haba + 250);
+		 theStage.setHeight(scene_takasa + 250);
+         
          // ルートのコンテナ
         StackPane root = new StackPane();
         
         // シーンの生成
         Scene scene = new Scene(root, 300, 250);
+         
+        Ball ball1 = new Ball();
+		ball1.ball.setFill(Color.GREEN);
+		root.getChildren().add(ball1.ball);
+		ball1.animation.setCycleCount(Timeline.INDEFINITE);
+		ball1.animation.play();
 
         // シーンをステージに貼る
         stage.setScene(scene);
