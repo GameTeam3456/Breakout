c5610003c666b0cc471d8cfc8a89a370cf762bcf
satosato0502 <kobuta.0502.sato@gmail.com>
2017-04-09 23:17:56 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,44 +1,38 @@
+package application;
+	
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class Main extends Application{
-    
-    static int scene_takasa = 300;
-	static int scene_haba = 250;
-    
-    public static void main(String[] args){
-        
-       launch(args);　//JavaFXのスレッドを起動
-       //startメソッドの呼び出しはJavaFXランタイムが自動的に実行
-        
-    }  
-           
-     public void start(Stage theStage){
-        theStage.setTitle("BreakOut");
-         
-         theStage.setWidth(scene_haba + 250);
-		 theStage.setHeight(scene_takasa + 250);
-         
-         // ルートのコンテナ
-        StackPane root = new StackPane();
-        
-        // シーンの生成
-        Scene scene = new Scene(root, 300, 250);
-         
-        Ball ball1 = new Ball();
-		ball1.ball.setFill(Color.GREEN);
-		root.getChildren().add(ball1.ball);
-		ball1.animation.setCycleCount(Timeline.INDEFINITE);
-		ball1.animation.play();
 
-        // シーンをステージに貼る
-        stage.setScene(scene);
-        
-        theStage.show();
-         
-         
-    }
+public class Main extends Application {
+	static int scene_takasa = 500;
+	static int scene_haba = 500;
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			primaryStage.setWidth(scene_haba + 250);
+			primaryStage.setHeight(scene_takasa + 250);
+			Group root = new Group();
+			Scene scene = new Scene(root,scene_takasa,scene_haba);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Ball ball1 = new Ball();
+			ball1.ball.setFill(Color.GREEN);
+			root.getChildren().add(ball1.ball);
+			ball1.animation.setCycleCount(Timeline.INDEFINITE);
+			ball1.animation.play();
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
-    
-        
-    
